4f473d6bf4b030d4aeb727ed6b1d14a504fee155
megdimka <megdimka@vivaldi.net>
2019-02-28 20:21:11 +0200

    upd

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ $(function() {
     text: "Загрузка домашки..."
   });
   load_hw();
-  $('[data-netlify-identity-button]').button();
+  $('[data-netlify-identity-button] a').button();
   if ((navigator.serviceWorker != null) && false) {
     navigator.serviceWorker.register('/sw.js').catch(function(e) {
       console.error(e);
