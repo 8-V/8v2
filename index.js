867905071f4d79cbb149ab2f4d7af41b23737d22
8-V <45600990+8-V@users.noreply.github.com>
2019-03-04 18:37:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var calc_food, clear_cache, init_chat, load_hw, role_change;
 
 getDate = function(d) {
-  d = Date.parse(d)
+  d = new Date(d)
   return [d.getDate(), d.getMonth() + 1].join('/')
 }
 
