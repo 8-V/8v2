c6496890807c107be2dc91c48e8736e8cb37502c
megdimka <megdimka@vivaldi.net>
2019-03-12 17:09:36 +0200

    add img

--- index.js
+++ index.js
@@ -1,126 +1,160 @@
-getDate = d => {
+var user_action = () => {}
+
+var getDate = /**
+ * @param {string | number | Date} d
+ */ d => {
   console.log(d)
   d = new Date(d)
   return [d.getDate(), d.getMonth() + 1].join('/')
 }
 
-clear_cache = () => {
+var clear_cache = async () => {
   if (navigator.serviceWorker == null) {
-    console.error('Cant remove cache');
-    return;
+    console.error('Cant remove cache')
+    return
   }
-  return navigator.serviceWorker.getRegistrations().then(x => {
-    for (i of x) {
-      i.unregister();
-    }
-  }).catch(e => {
-    console.error(e);
-  });
-};
+  try {
+    const x = await navigator.serviceWorker.getRegistrations()
+    for (var i of x) i.unregister()
+  } catch (e) {
+    console.error(e)
+  }
+}
 
-load_hw = () => {
-  fetch('https://homework-63c7.restdb.io/rest/email_inbound', {
+var load_hw = async () => {
+  return fetch('https://homework-63c7.restdb.io/rest/email_inbound', {
     method: 'GET',
     headers: {
       'x-apikey': '5c6ecf1828ca2e129e8696e8'
     }
-  }).then(x => x.json()).then(predmets => {
-    if('message' in predmets) {
-      $.unblockUI();
-      $.blockUI({message: "Внимание! Сервер заспамлен, сайт временно закрыт."})
-    }
-    for (predmet of predmets) {
-      hw2 = predmet.from == 'khorsun_dv@dlit.dp.ua'
-      hw1 = predmet.from == 'anton.gimnasium@gmail.com'
-      x = $(`<div data-role="collapsible" data-filtertext="${predmet.subject}">`).html(`<h3>${predmet.subject}</h3>${predmet.body}`)
-      if (hw2) x.appendTo($('#hw2'))
-      else if (hw1) x.appendTo($('#hw1'))
-      else x.appendTo($('#cool'))
-    }
-    $('#hw1 #hw2').collapsibleset('refresh');
-    $.unblockUI();
-  }).catch(e => {
-    console.error(e);
-  });
-};
+  })
+    .then(x => x.json())
+    .then(predmets => {
+      if ('message' in predmets) {
+        $.unblockUI()
+        $.blockUI({
+          message: 'Внимание! Сервер заспамлен, сайт временно закрыт.'
+        })
+      }
+      for (var predmet of predmets) {
+        var hw2 = predmet.from == 'khorsun_dv@dlit.dp.ua'
+        var hw1 = predmet.from == 'anton.gimnasium@gmail.com'
+        var x = $(
+          `<div data-role="collapsible" data-filtertext="${predmet.subject}">`
+        ).html(`<h3>${predmet.subject}</h3>${predmet.body}`)
+        if (hw2) x.appendTo($('#hw2'))
+        else if (hw1) x.appendTo($('#hw1'))
+        else x.appendTo($('#cool'))
+      }
+      $('#hw1 #hw2').collapsibleset('refresh')
+      $.unblockUI()
+    })
+    .catch(e => {
+      console.error(e)
+    })
+}
 
-init_chat = () => { };
+var init_chat = () => {}
 
-calc_food = () => {
-  result = $('#food-result');
-  count_by = {
+var calc_food = () => {
+  var result = $('#food-result')
+  var count_by = {
     '0': 0,
     '5': 0,
     '30': 0,
     '35': 0
-  };
-  count = 0;
-  ref = $('#food-group').children().children().toArray();
-  for (i of ref) {
-    i = i.childNodes[1];
-    if (!i.checked) {
-      count_by[i.name]++;
+  }
+  var group = $('#food-group')
+    .children()
+    .children()
+    .toArray()
+  for (var chbox of group) {
+    chbox = chbox.childNodes[1]
+    if (!chbox.checked) {
+      count_by[chbox.name]++
     }
   }
-  console.log(count_by);
-  price = count_by['5'] * 5 + count_by['30'] * 30 + count_by['35'] * 35;
-  result.html(`<p>${count_by['0'] + count_by['5'] + count_by['30'] + count_by['35']} человек</p><p>${count_by['0']} бесплатников</p><p>${count_by['5']} по 5 грн</p><p>${count_by['30']} по 30 грн</p><p>${count_by['35']} по 35</p><p>Итого ${price} грн.</p>`);
-};
+  console.log(count_by)
+  var price = count_by['5'] * 5 + count_by['30'] * 30 + count_by['35'] * 35
+  result.html(
+    `<p>${count_by['0'] +
+      count_by['5'] +
+      count_by['30'] +
+      count_by['35']} человек</p><p>${count_by['0']} бесплатников</p><p>${
+      count_by['5']
+    } по 5 грн</p><p>${count_by['30']} по 30 грн</p><p>${
+      count_by['35']
+    } по 35</p><p>Итого ${price} грн.</p>`
+  )
+}
 
-role_change = () => {
-  var role, role_friendly_names, role_icons, role_urls, user_action_btn;
-  user_action_btn = $('#user_action');
-  user_action_btn.hide();
-  role = localStorage.role;
+var role_change = () => {
+  var role, role_friendly_names, role_icons, role_urls, user_action_btn
+  user_action_btn = $('#user_action')
+  user_action_btn.hide()
+  role = localStorage.role
   role_friendly_names = {
-    'role_leader': 'Чат старостата',
-    'role_prefect': 'Чат старостата',
-    'role_culture': 'Чат старостата',
-    'role_food': 'Счетчик порций'
-  };
+    role_leader: 'Чат старостата',
+    role_prefect: 'Чат старостата',
+    role_culture: 'Чат старостата',
+    role_food: 'Счетчик порций'
+  }
   role_urls = {
-    'role_leader': '#chat',
-    'role_prefect': '#chat',
-    'role_culture': '#chat',
-    'role_food': '#food_calc'
-  };
+    role_leader: '#chat',
+    role_prefect: '#chat',
+    role_culture: '#chat',
+    role_food: '#food_calc'
+  }
   role_icons = {
-    'role_leader': 'comment',
-    'role_prefect': 'comment',
-    'role_culture': 'comment',
-    'role_food': 'user'
-  };
-  $('#' + role).click().click();
+    role_leader: 'comment',
+    role_prefect: 'comment',
+    role_culture: 'comment',
+    role_food: 'user'
+  }
+  $('#' + role)
+    .click()
+    .click()
   if (role !== 'role_default') {
-    user_action_btn.attr('href', role_urls[role]);
-    user_action_btn.html(role_friendly_names[role]);
+    user_action_btn.attr('href', role_urls[role])
+    user_action_btn.html(role_friendly_names[role])
     user_action_btn.buttonMarkup({
       icon: role_icons[role]
-    });
-    user_action_btn.show();
-  } else user_action_btn.hide();
-};
+    })
+    user_action_btn.show()
+  } else user_action_btn.hide()
+}
 
 $(() => {
-  var child, j, len, ref;
-  $.blockUI({ message: 'Загрузка домашки...' });
-  load_hw();
+  var child, j, len, ref
+  $.blockUI({ message: 'Загрузка домашки...' })
+  load_hw()
   if (localStorage.role == null) {
-    localStorage.role = 'role_default';
+    localStorage.role = 'role_default'
   }
   $('input[type=radio][name=role]').change(() => {
-    localStorage.role = $('input[type=radio][name=role]').toArray().filter(x=>x.checked)[0].id || 'role_default';
-    role_change();
-  });
-  $('#settings div a[data-icon=back]').on('click', user_action);
+    localStorage.role =
+      $('input[type=radio][name=role]')
+        .toArray()
+        .filter(
+          /**
+           * @param {{ checked: any; }} x
+           */
+          x => x.checked
+        )[0].id || 'role_default'
+    role_change()
+  })
+  $('#settings div a[data-icon=back]').on('click', user_action)
   $(document).on('swiperight', '.ui-page', () => {
-    $('#settings-panel').panel('open');
-  });
-  $('a[href=#chat]').on('click', init_chat);
-  ref = $('#food-group').controlgroup().children().children();
+    $('#settings-panel').panel('open')
+  })
+  $('a[href=#chat]').on('click', init_chat)
+  ref = $('#food-group')
+    .controlgroup()
+    .children()
+    .children()
   for (j = 0, len = ref.length; j < len; j++) {
-    child = ref[j];
-    $(child).on('change', calc_food);
+    child = ref[j]
+    $(child).on('change', calc_food)
   }
-  role_change();
-});
+  role_change()
+})
--- sw.js
+++ sw.js
@@ -1,39 +1,42 @@
-var cacheName = '4';
-var filesToCache = ['/', '/index', '/index.html', '/index.js', '/favicon.ico'];
+var cacheName = '4'
+var filesToCache = ['/', '/index', '/index.html', '/index.js', '/favicon.ico']
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(caches.open(cacheName)
-    .then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    }));
-});
-self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
+self.addEventListener('install', e => {
+  console.log('[ServiceWorker] Install')
   e.waitUntil(
-    caches.keys()
-    .then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
+    caches.open(cacheName).then(cache => {
+      console.log('[ServiceWorker] Caching app shell')
+      return cache.addAll(filesToCache)
     })
-    .catch(function(e) {
-      console.error(e);
-    })
-  );
-  return self.clients.claim();
-});
+  )
+})
+self.addEventListener('activate', e => {
+  console.log('[ServiceWorker] Activate')
+  e.waitUntil(
+    caches
+      .keys()
+      .then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== cacheName) {
+              console.log('[ServiceWorker] Removing old cache', key)
+              return caches.delete(key)
+            }
+          })
+        )
+      })
+      .catch(e => {
+        console.error(e)
+      })
+  )
+  return self.clients.claim()
+})
 
-self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+self.addEventListener('fetch', e => {
+  console.log('[ServiceWorker] Fetch', e.request.url)
   e.respondWith(
-    caches.match(e.request)
-    .then(function(response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request)
     })
-  );
-});
+  )
+})
