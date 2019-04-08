a543d6288819b9113bdf33ab07e091689ee168c6
megdimka <megdimka@vivaldi.net>
2019-04-08 18:45:47 +0300

    upd

--- sw.js
+++ sw.js
@@ -1,62 +1,53 @@
-const cacheName = 'v0m'
-const toCache = [
-  '/',
-  '/index.js',
-  '/favicon.ico'
-]
-const toBackup = [
-  'https://homework-63c7.restdb.io/rest/email_inbound'
-]
+const cacheName = 'v1.0.2';
+const toCache = ['/', '/index.js', '/favicon.ico'];
+const toBackup = ['https://homework-63c7.restdb.io/rest/email_inbound'];
 
 self.addEventListener('install', evt => {
-  caches.open(cacheName).then(cache => cache.addAll(toCache))
-  evt.waitUntil(self.skipWaiting())
-})
+  caches.open(cacheName).then(cache => cache.addAll(toCache));
+  evt.waitUntil(self.skipWaiting());
+});
 
 self.addEventListener('activate', evt => {
-  console.log('[sw] activate')
+  console.log('[sw] activate');
   evt.waitUntil(
     caches.keys().then(keys => {
       Promise.all(
         keys
-        .filter(key => cacheName != key)
-        .map(key => {
-          console.log('[sw] delete', key)
-          caches.delete(key)
-        })
-      )
+          .filter(key => cacheName != key)
+          .map(key => {
+            console.log('[sw] delete', key);
+            caches.delete(key);
+          })
+      );
     })
-  )
-})
+  );
+});
 
 self.addEventListener('fetch', evt => {
-  console.log('[sw] fetch', evt.request.url)
+  console.log('[sw] fetch', evt.request.url);
   if (toBackup.includes(evt.request.url)) {
-    console.log('[sw] Backing up', evt.request.url)
+    console.log('[sw] Backing up', evt.request.url);
     evt.respondWith(
       fetch(evt.request)
-      .then(
-        async resp => {
-          cache = await caches.open(cacheName)
-          cache.put(evt.request, resp.clone())
-          return resp
-        }
-      )
-      .catch(_ => caches.match(evt.request))
-    )
+        .then(async resp => {
+          cache = await caches.open(cacheName);
+          cache.put(evt.request, resp.clone());
+          return resp;
+        })
+        .catch(_ => caches.match(evt.request))
+    );
   } else {
     evt.respondWith(
-      caches.match(evt.request).then(
-        resp => {
-          return resp || fetch(evt.request).then(
-            async res => {
-              cache = await caches.open(cacheName);
-              cache.put(evt.request, res.clone());
-              return res;
-            }
-          )
-        }
-      )
-    )
+      caches.match(evt.request).then(resp => {
+        return (
+          resp ||
+          fetch(evt.request).then(async res => {
+            cache = await caches.open(cacheName);
+            cache.put(evt.request, res.clone());
+            return res;
+          })
+        );
+      })
+    );
   }
-})
+});
