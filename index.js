122172fd638d489b2a2ccbc6d3603383ef293183
teki <teki@asd.f>
2019-05-09 20:55:42 +0300

    upd

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var parse_hw = (hw1, hw2) => {
-  for (var predmet of hw1) {
-    var x = $(
+let parse_hw = (hw1, hw2) => {
+  for (let predmet of hw1) {
+    let x = $(
       `<div data-role="collapsible" data-filtertext="${predmet.subject}">`,
     ).html(`<h3>${predmet.subject}</h3>${predmet.body}`);
     x.appendTo($('#hw1'));
   }
-  for (var predmet of hw2) {
-    var x = $(
+  for (let predmet of hw2) {
+    let x = $(
       `<div data-role="collapsible" data-filtertext="${predmet.subject}">`,
     ).html(`<h3>${predmet.subject}</h3>${predmet.body}`);
     x.appendTo($('#hw2'));
@@ -14,14 +14,14 @@ var parse_hw = (hw1, hw2) => {
   return true;
 };
 
-var load_hw = async () => {
-  var hw1 = await fetch('https://homework-63c7.restdb.io/rest/email_inbound', {
+let load_hw = async () => {
+  let hw1 = await fetch('https://homework-63c7.restdb.io/rest/email_inbound', {
     method: 'GET',
     headers: {
       'x-apikey': '5c6ecf1828ca2e129e8696e8',
     },
   }).then(x => x.json());
-  var hw2 = await fetch('https://homework-63c7.restdb.io/rest/hw', {
+  let hw2 = await fetch('https://homework-63c7.restdb.io/rest/hw', {
     method: 'GET',
     headers: {
       'x-apikey': '5c6ecf1828ca2e129e8696e8',
@@ -40,25 +40,22 @@ var load_hw = async () => {
   $.unblockUI();
 };
 
-var calc_food = () => {
-  var result = $('#food-result');
-  var count_by = {
+let calc_food = () => {
+  let result = $('#food-result');
+  let count_by = {
     '0': 0,
     '5': 0,
     '30': 0,
     '35': 0,
   };
-  var group = document.querySelectorAll('#food-group input');
-  // console.dir(group)
+  let group = document.querySelectorAll('#food-group input');
   group.forEach(chbox => {
-    // console.log(chbox)
     if ('undefined' != typeof chbox && chbox.checked);
     else {
       count_by[chbox.name]++;
     }
   });
-  // console.log(count_by)
-  var price = count_by['5'] * 5 + count_by['30'] * 30 + count_by['35'] * 35;
+  let price = count_by['5'] * 5 + count_by['30'] * 30 + count_by['35'] * 35;
   result.html(
     `<p>${count_by['0'] +
       count_by['5'] +
@@ -71,49 +68,12 @@ var calc_food = () => {
   );
 };
 
-var role_change = () => {
-  var user_action_btn = $('#user_action');
-  user_action_btn.hide();
-  var role = localStorage.role;
-  var role_friendly_names = {
-    food: 'Счетчик порций',
-  };
-  var role_urls = {
-    food: '#food',
-  };
-  var role_icons = {
-    food: 'user',
-  };
-  if (role !== 'default') {
-    user_action_btn.attr('href', role_urls[role]);
-    user_action_btn.html(role_friendly_names[role]);
-    user_action_btn.buttonMarkup({
-      icon: role_icons[role],
-    });
-    user_action_btn.show();
-  } else {
-    user_action_btn.hide();
-  }
-};
-
 $(() => {
   if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');
   $.blockUI({
     message: 'Загрузка домашки...',
   });
   load_hw();
-  if (localStorage.role == null) {
-    localStorage.role = 'default';
-  }
-  $('input[name=role]').change(() => {
-    role = document.querySelector('.ui-checkbox-off + [name=role]');
-    if (!role)
-      role = {
-        id: 'default',
-      };
-    localStorage.role = role.id;
-    role_change();
-  });
   $(document).on('swiperight', '.ui-page', () => {
     if ($.mobile.activePage.attr('id') == 'main')
       $('#settings-panel').panel('open');
@@ -123,9 +83,14 @@ $(() => {
         reverse: true,
       });
   });
-  var group = $('#food-group')[0].form;
-  for (var child of group) {
+  let group = $('#food-group')[0].form;
+  for (let child of group) {
     $(child).on('change', calc_food);
   }
-  role_change();
+  $('#theme').on('click', _ => {
+    $('.ui-mobile-viewport').toggleClass('ui-overlay-b');
+    $('[data-role=page]').toggleClass('ui-page-theme-b');
+    $('[data-role=header], [data-role=listview] > li').toggleClass('ui-bar-b');
+    $('ui-btn').toggleClass('ui-btn-b');
+  });
 });
