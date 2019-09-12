c0b78826b686c7bd3c4831e277c34f0a4ae2c3b5
tekirinka <45600990+tekirinka@users.noreply.github.com>
2019-09-12 16:10:25 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,2 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
-
-
+//importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+self.addEventListener('fetch',evt => {evt.respondWith(fetch(evt.request))});
